2f060a0b55b899981e08a26f333fb8f7d2933b98
westkite2 <westkite2@gmail.com>
2022-03-12 16:48:37 +0900

    First commit, setup objects.

---
+++ Assets/Scripts/FrogController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrogController : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+        
+
+    }
+
+    void Update()
+    {
+        transform.Translate(0, 0, 0.01f);
+        
+    }
+
+
+
+   
+}
