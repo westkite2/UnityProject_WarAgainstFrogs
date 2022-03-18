508a6592f56a303e1d14d8583e0f697e66810940
westkite2 <westkite2@gmail.com>
2022-03-18 18:46:45 +0900

    feat: Add frog animation - swim, jump

--- Assets/Scripts/FrogController.cs
+++ Assets/Scripts/FrogController.cs
@@ -4,21 +4,56 @@ using UnityEngine;
 
 public class FrogController : MonoBehaviour
 {
+    public GameObject frog;
+    public GameObject guts;
+    private Animator animator;
+    private bool smashed = false;
+
+    //Flags
+    public bool isSwimming = true;
+    public bool isJumping = false;
+
+    //Jump
+    public Vector3 jumpPos; //Jump starting position
+    public Vector3 landPos; //Jump landing position    
+    public float reduceHeight = 1f; //center value of the jump parabola (higher the lower)
+    public float journeyTime = 2f; //Duration from jumpPos to landPos (higher the slower)
+    public float startTime;
+
+    private void Awake()
+    {
+        animator = frog.GetComponent<Animator>();
+
+    }
 
     void Start()
     {
         
-        
-
     }
 
     void Update()
     {
-        transform.Translate(0, 0, 0.01f);
-        
+        if (isSwimming)
+        {
+            transform.Translate(0, 0, 0.01f);
+        }
+        if (isJumping)
+        {
+            Jump();
+        }
+
     }
 
-
-
-   
+    //Custom Functions
+    public void Jump()
+    {
+        animator.SetBool("Jump", true);
+        Vector3 center = (jumpPos + landPos) * 0.5F; //gose up by center value
+        center -= new Vector3(0, 1f * reduceHeight, 0); //higher y the lower
+        Vector3 jumpCenter = jumpPos - center;
+        Vector3 landCenter = landPos - center;
+        float fracComplete = (Time.time - startTime) / journeyTime;
+        transform.position = Vector3.Slerp(jumpCenter, landCenter, fracComplete);
+        transform.position += center;
+    }
 }
--- Assets/Scripts/JumpZone.cs
+++ Assets/Scripts/JumpZone.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class JumpZone : MonoBehaviour
 {
+    public GameObject frog;
+    public GameObject ship;
+    FrogController FrogControllerScript;
+
     // Start is called before the first frame update
     void Start()
     {
+        FrogControllerScript = frog.GetComponent<FrogController>();
         
     }
 
@@ -18,6 +23,12 @@ public class JumpZone : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        
+        FrogControllerScript.isSwimming = false; 
+        FrogControllerScript.isJumping = true;
+        FrogControllerScript.startTime = Time.time;
+        FrogControllerScript.jumpPos = frog.transform.position;
+        FrogControllerScript.landPos = new Vector3(frog.transform.position.x, 0.3f, ship.transform.position.z);
     }
+
+
 }
